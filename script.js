9a95a703ea13bf499e693314760b91024d3b05d3
Shitalkalhapure <kalhapuresh@gmail.com>
2026-04-20 21:11:53 +0530

    variables

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("This is my first java script program")
+console.log("This is my second java script program")
+let fName="Shitalk";
+const pi=3.17;
+var phoneno=12345678;
+console.log(fName);
+console.log(pi)
+console.log(phoneno);
+ fName="Neil";
+ //pi=4.17;
+ phoneno=98765;
+console.log(fName);
+console.log(pi)
+console.log(phoneno);
